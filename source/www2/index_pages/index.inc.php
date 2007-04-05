7b1db795f79de8ff3872e402f6dcbc1f9b84fc99
ljpnet <ljpnet@2f34e94d-2c2d-0410-b9d0-e77c540a6212>
2007-04-05 09:57:25 +0000

    only index-3.php in htdoc root, css,images all move to index_pages/

--- source/www2/index_pages/index.inc.php
+++ source/www2/index_pages/index.inc.php
@@ -1,8 +1,16 @@
 <?php
 
+//首页图片的url地址，相对目录的话要从index所在的目录算。
 $img_src="index_pages/20070402-spring/spring-".(time()%8).'.jpg';
+
+//图片链接到的地方，设为空或不设，则用link_board变量，链接到一个版面
+//$link_to='http://code.google.com';
+
+//链接到的版面
 $link_board='travel';
 
+// </head>之前要附加的内容，一般不要设，除非你知道你在干什么 :)
 //$extra_head='';
+
 
 ?>
