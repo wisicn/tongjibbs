21e0fe664b5209ba84bbb834c672f3d4f1a9290e
ljpnet <ljpnet@2f34e94d-2c2d-0410-b9d0-e77c540a6212>
2007-04-07 17:35:57 +0000

    update index.php(add js, add more custom content, add wForum option, and fix some css bug)

--- source/www2/index.php
+++ source/www2/index.php
@@ -1,22 +1,22 @@
 <?php
 /*
 
-说明 见 index_pages/index.inc.php 文件
+同舟共济BBS首页，自定义内容 见 index_pages/index.inc.php 文件
 
 ======================================================
 todo: 
 
 考虑index_pages页面文件可能有错误，容错性
 css修改成方便改背景色的
-xhtml验证
-done (W3C CSS 校验通过)css 验证
+xxx done  xhtml验证
+xxx done (W3C CSS 校验通过)css 验证
 美工美化
 telnet教程的链接
 fterm cterm下载的链接
 【站长提示】和【推荐链接】以及其他要在首页的显示的内容
 
-加入wFroum选择处理
-点击版面会自动根据选择进入经典web或wForum的版面
+xxx done 加入wFroum选择处理
+xxx done 点击版面会自动根据选择进入经典web或wForum的版面
 
 进一步模块化，规范化，做到可方便切换themes
 
@@ -27,15 +27,19 @@ $index_pages='index_pages/';
 
 
 require("www2-funcs.php");
-require($index_pages."index.inc.php");
 
 function link_board($eng,$chs)
 {
+  //TODO:自动读取中文版名
   global $index_pages;
-  $burl='frames.html?mainurl=/bbsdoc.php?board=';
+  $burl='frames.html?mainurl=/bbsdoc.php?board=';//理论上讲这个只是显示来看看的，和给爬虫进来用的，实际是goboard函数控制地址的
+  //'wForum/frames.php?target=board.php%3Fname=';
   echo "<h2 class='dotmark'>";
   echo "<a href=\"{$burl}$eng\" onclick=\"return goboard('$eng');\">$chs</a></h2> \n";
 }
+
+require($index_pages."index.inc.php");
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="zh-CN" lang="zh-CN">
@@ -44,30 +48,7 @@ function link_board($eng,$chs)
 <meta http-equiv="content-type" content="text/html; charset=gb2312" />
 <meta name="author" content="老林(http://www.laolin.com)" />
 <link href="<?php echo $index_pages ?>index.css" type="text/css" rel="stylesheet" />
-<!--script src="index.js" type="text/javascript"></script-->
-<script type="text/javascript">
-
-
-function guestlogin()
-{
-document.form1.id.value="guest";
-document.form1.submit();
-}
-function register()
-{
-top.location.href="bbsreg0.html";
-}
-        
-function dosubmit(o)
-{
-//TODO:处理wForum选择
-    return true;
-}
-function goboard()
-{
- //TODO:处理wForum选择
-}
-</script>
+<script src="<?php echo $index_pages ?>index.js" type="text/javascript"></script>
 <?php if(isset($extra_head)) echo $extra_head; ?>
 </head>
 <body  onload="document.form1.id.focus()">
@@ -76,33 +57,35 @@ function goboard()
   	
      <div id="mainimage">
      <!--在图片前后加个高350的div，使得图片未下载时，高度位置先空着 -->
-     <div style="float:left;height:350px;width:1px;" /></div>
+     <div style="float:left;height:350px;width:1px;"></div>
      <?php 
      if(isset($link_to) && $link_to!='')
        $urlto=$link_to;
      else
        $urlto='/frames.html?mainurl=/bbsdoc.php?board='.$link_board;
        ?>
-       <a href="<?php echo $urlto;?>"><img src="<?php echo $img_src; ?>" alt="<?php echo $link_board;?> board" /></a>
+       <a href="<?php echo $urlto;?>"
+       <?php if(!isset($link_to) || $link_to=='')echo "onclick=\"return goboard('{$link_board}')\"";?>
+       ><img src="<?php echo $img_src; ?>"  alt="<?php echo $link_tip;?>" title="<?php echo $link_tip;?>" /></a>
      <div style="clear:both;"></div>
      </div><!-- mainimage -->
      
-     <div>
-     </div>
+     <?php if(isset($extra_html_after_img)&&$extra_html_after_img!='')
+     echo '<div class="cbox">'.$extra_html_after_img.'</div>';?>
      
      <div id="mainform">
 
  	<form id="form1" name="form1" action="/bbslogin.php" method="post"
- 	 onsubmit="return dosubmit(this)">
+ 	 onsubmit="return before_submit()">
 	  用户名：<input type="text" name="id" id="id" class="u"
 	  onmouseover="this.focus()" onfocus="this.select()" 
 	  onkeydown="if(event.keyCode==13)event.keyCode=9" />
 	  密码：<input type="password" name="passwd" id="passwd" class="p"  
 	   maxlength="39" />
-    <input type="radio" class="r" id="we" name="webselect2"
-     value="0"  checked="checked" /><label for="we">经典web</label>
-    <input type="radio" class="r" id="wf" name="webselect2" disabled="disabled"
-     value="1" /><label for="wf" title="对不起，wForum正在调试中">wForum</label>
+    <input type="radio" class="r" id="we" name="webselect[]"
+     value="0"  checked="checked" /><label for="we" title="经典WEB,和TELNET比较接近">经典web</label>
+    <input type="radio" class="r" id="wf" name="webselect[]" no_disabled="disabled"
+     value="1" /><label for="wf" title="wForum,和一般网上论坛比较接近">wForum</label>
 	  <input type="hidden" value="/wForum/frames.php" name="comeurl" />
 	  <input type="submit" name="logon" class="b" id="logon" value="登录" />
 	  <input type="button" name="guest" id="guest" value="匿名"  class="b"
@@ -113,6 +96,8 @@ function goboard()
                 
      </div><!-- mainform -->
   
+     <div id="errorinfo"></div><!-- 提示信息 -->
+
      <div id="onlineinfo">目前在线人数<?php echo '['.bbs_getonlinenumber(),']('.bbs_getwwwguestnumber().' WWW GUEST)';?>
      </div><!-- onlineinfo -->
 
@@ -135,6 +120,9 @@ link_board("Undergraduate","本科生之家" );
 ?>
      	
      </div><!-- maintip -->
+     
+     <?php if(isset($extra_html_before_footer)&& $extra_html_before_footer!='' )
+     echo '<div class="cbox">'.$extra_html_before_footer.'</div>';?>
      
      <div id="footer">
 <hr/>
