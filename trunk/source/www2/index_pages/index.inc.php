d1d68aff0870760657dc0231f4edaa55c4a003ea
ljpnet <ljpnet@2f34e94d-2c2d-0410-b9d0-e77c540a6212>
2007-04-07 17:35:57 +0000

    update index.php(add js, add more custom content, add wForum option, and fix some css bug)

--- trunk/source/www2/index_pages/index.inc.php
+++ trunk/source/www2/index_pages/index.inc.php
@@ -1,16 +1,28 @@
 <?php
 
-//首页图片的url地址，相对目录的话要从index所在的目录算。
+
+//1 首页图片的url地址，相对目录的话要从index所在的目录算。根据同舟共济原来的习惯，建议使用700x350的尺寸
 $img_src="index_pages/20070402-spring/spring-".(time()%8).'.jpg';
 
-//图片链接到的地方，设为空或不设，则用link_board变量，链接到一个版面
-//$link_to='http://code.google.com';
+//2 鼠标放在图片上提示的内容
+$link_tip='春游啦～～～'; //注：此字符串变量不可以有双引号。
 
-//链接到的版面
-$link_board='travel';
+//3a 图片链接到的地方，设为空或不设，则用link_board变量，链接到一个版面
+//$link_to='http://code.google.com'; //注：此字符串变量不可以有双引号。
 
-// </head>之前要附加的内容，一般不要设，除非你知道你在干什么 :)
+//3b 链接到的版面
+$link_board='travel'; //注：此字符串变量不可以有双引号。
+
+
+
+//e1 </head>之前要附加的内容，一般不要设，除非你知道你在干什么 :)
 //$extra_head='';
+
+//e2 显示在图片下方的额外HTML内容 (默认文字是居中的) 
+//$extra_html_after_img='test ';
+
+//e3 显示在页脚长横线上方的额外HTML内容
+//$extra_html_before_footer='<div style="text-align:left;">2 test aa a BBBBBBBBBBa</div>';
 
 
 ?>
---
+++ trunk/source/www2/index_pages/index.js
@@ -0,0 +1,78 @@
+function guestlogin()
+{
+document.form1.id.value="guest";
+before_submit();
+document.form1.submit();
+}
+function register()
+{
+top.location.href="bbsreg0.html";
+}
+
+/* ==========================================
+根据web/wForum选择 自动登录不同的界面
+
+注：调用此函数的地方应保证有 id='wf'的东西在
+
+TODO:提交前先判断一下提交表单的合理性
+*/
+function before_submit()
+{
+  is_wforum=document.getElementById('wf').checked;
+  
+  u_id=document.getElementById('id').value;
+  u_passwd=document.getElementById('passwd').value;
+  
+  haserror=false;
+  einfo='';
+
+  
+  if(u_id=='')
+  {
+    haserror=true;
+    einfo+='<br/>请输入用户名';
+  }
+  if(u_passwd=='')
+  {
+    haserror=true;
+    einfo+='<br/>请输入密码';
+  }
+  if(haserror)
+  {
+    ediv=document.getElementById('errorinfo');
+    ediv.innerHTML='错误信息:<b>'+einfo+'</b>';
+    ediv.style.display='block';
+    return false;
+  }
+  
+  f=document.getElementById('form1');
+  if(is_wforum)
+    f.action='wForum/logon.php';
+  else
+    f.action='bbslogin.php';
+  return true;
+}
+
+/* ==================================================
+根据web/wForum选择自动进入不同界面的版面
+
+要进入版面的链接除了href='xxxxx'外，还加了 onclick="return goboard('xxxx')
+这样实际上javascript处理的跳转的地方，href的地址被忽略了。
+
+实现了根据选择进入不面界面的版面
+*/
+function goboard(bd)
+{
+  o=document.getElementById('wf');
+  if(!o)is_wforum=false;
+  else 
+  is_wforum=o.checked;
+  if(is_wforum)
+    path="wForum/frames.php?target=board.php%3Fname=";
+  else
+    path='frames.html?mainurl=/bbsdoc.php?board=';
+  top.location.href=path+bd;
+
+  return false;//false to ignore the origin link uri.
+}
+
